1d9af1893fef70f7fda1ae1fe0ab89cbf8730aaf
tuyen-nnt <tuyenthanh9116@gmail.com>
2021-08-15 02:09:32 +0700

    add more pages and update

--- js/scripts.js
+++ js/scripts.js
@@ -51,4 +51,47 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+
+
+    function searchToggle(obj, evt){
+        var container = $(obj).closest('.search-wrapper');
+        if(!container.hasClass('active')){
+            container.addClass('active');
+            evt.preventDefault();
+        }
+        else if(container.hasClass('active') && $(obj).closest('.input-holder').length == 0){
+            container.removeClass('active');
+            // clear input
+            container.find('.search-input').val('');
+        }
+    }
+
 });
+
+    // Sign-up
+
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (function() {
+        'use strict';
+        window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+        form.classList.add('was-validated');
+    }, false);
+ });
+}, false);
+})();
+
+    // Share form
+    $(function () {
+        $("#editor").shieldEditor({
+            height: 260
+        });
+    })
